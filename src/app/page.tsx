b8d79345d369a0e9d90bbb8fb6a4a090c2a3f4f1
Gabriel <gabrielinac22@gmail.com>
2025-10-17 16:06:35 -0300

    Cria página inicial importando seções principais do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Hero from '@/components/home/Hero';
-import ServiceCards from '@/components/home/ServiceCards';
-import StatsSection from '@/components/home/StatsSection';
-import ClientLogos from '@/components/home/ClientLogos';
+import ClientLogos from "@/components/home/ClientLogos";
+import Hero from "@/components/home/Hero";
+import ServiceCards from "@/components/home/ServiceCards";
+import StatsSection from "@/components/home/StatsSection";
 
 export default function Home() {
   return (
